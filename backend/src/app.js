7015e8766e355cde6c48c6e22c96d1f3f019e447
rickyyy72 <ricky.weng89@gmail.com>
2020-03-13 17:08:29 -0400

    Fixed the registration form so the correct data is sent #57

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,8 +18,6 @@ app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(path.join(__dirname, '../public')));
 app.set('view engine', 'ejs');
 
-console.log(__dirname)
-
 app.use(activateRoute);
 app.use(registerRoute);
 
