0f0dc1fe428ff91ae494c524b7552d38c1f58ebe
rickyyy72 <ricky.weng89@gmail.com>
2020-03-24 11:09:26 -0400

    Setup express-session #53
    We are using sessions to store the user when they login so we know if they're logged in and who the user is

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const session = require('express-session');
 require('dotenv').config();
 require('./database/mysql');
 const activateRoute = require('./routes/activate');
@@ -13,6 +14,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            sameSite: 'lax',
+            httpOnly: true,
+            maxAge: 60 * 60 * 1000
+        }
+    })
+);
 
 app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(path.join(__dirname, '../public')));
