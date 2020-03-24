8ed9570353d33f1bbc4623095aaa23422af577fd
rickyyy72 <ricky.weng89@gmail.com>
2020-03-24 17:17:43 -0400

    Added the stylesheet for the create course page #64

--- backend/src/routes/courses.js
+++ backend/src/routes/courses.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 router.post('/courses/create', CoursesController.createCourse);
 
+router.get('/courses/create', (req, res) => {
+  res.render('create_course')
+})
+
 router.get('/courses', (req, res) => {
   res.render('courses');
 })
