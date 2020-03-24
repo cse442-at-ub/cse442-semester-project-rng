2443aadfcff2c9f932d91da36d911da15863ddc7
rickyyy72 <ricky.weng89@gmail.com>
2020-03-24 16:29:01 -0400

    Refactored course related files
    Organized all courses related functionality into one file so it is easier to find courses related functions

--- backend/src/controller/courses.js
+++ backend/src/controller/courses.js
@@ -4,22 +4,21 @@ const bcrypt = require('bcrypt');
 
 const saltRounds = 10;
 
-module.exports.registerUser = async (req, res) => {
+module.exports.createCourse = async (req, res) => {
   //TODO: Fill with actuall id's once the form has been created;
   const instructor = req.body.instructor;
   const name = req.body.name;
   const term = req.body.term;
 
   const connection = await mysql.getConnection();
+  await connection.execute(
+    `INSERT INTO rng_courses (course_instructor, course_name, course_term, ) 
+                VALUES ("${instructor}" , "${name}" , "${term}")`
+  );
 
-  bcrypt.hash(password, saltRounds, async function(err, hash) {
-    await connection.execute(
-      `INSERT INTO rng_courses (course_instructor, course_name, course_term, ) 
-                  VALUES ("${instructor}" , "${name}" , "${term}")`
-    );
-  });
-
-  res.send(`Inserted user into DB`);
-
-  
+  res.send(`Inserted new course into DB`);
 };
+
+module.exports.enrollCourse = (req, res) => {
+
+}
