9a7161e978126dcf1c3b78bcbaf0922f2ff1933e
rickyyy72 <ricky.weng89@gmail.com>
2020-03-24 16:55:49 -0400

    Render the courses page when users visit the courses route #63

--- backend/src/routes/courses.js
+++ backend/src/routes/courses.js
@@ -1,8 +1,11 @@
-
 const express = require('express');
 const RegisterController = require('../controller/register');
 const router = express.Router();
 
 router.post('/course-create', RegisterController.registerUser);
+
+router.get('/courses', (req, res) => {
+  res.render('courses');
+})
 
 module.exports = router;
