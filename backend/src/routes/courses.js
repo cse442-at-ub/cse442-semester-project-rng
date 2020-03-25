8e49caf3d5928d0a397f08d8e2dd7bbeb991142f
rickyyy72 <ricky.weng89@gmail.com>
2020-03-24 23:45:00 -0400

    Only allow logged in users to visit the courses page #66

--- backend/src/routes/courses.js
+++ backend/src/routes/courses.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const auth = require('../auth/auth');
 const CoursesController = require('../controller/courses');
 const router = express.Router();
 
@@ -8,7 +9,7 @@ router.get('/courses/create', (req, res) => {
   res.render('create_course')
 })
 
-router.get('/courses', (req, res) => {
+router.get('/courses', auth.isLoggedIn, (req, res) => {
   res.render('courses');
 })
 
