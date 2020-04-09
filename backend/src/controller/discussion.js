b1c3355c07336948a1f5780f743f94877a51dd5e
Daniel Brittain <dmbritta@buffalo.edu>
2020-04-09 08:25:24 -0400

    #79 Added database information to the controller
    This is more or less complete. The only thing left to do is update the user field to actually be the user, which can't be done until I know what the page looks like.

--- backend/src/controller/discussion.js
+++ backend/src/controller/discussion.js
@@ -3,15 +3,23 @@ const bodyParser = require('body-parser');
 
 module.exports.registerUser = async (req, res) => {
     //TODO: Fill with actuall id's once the form has been created;
-    const val1 = req.body.val1;
-    const val2 = req.body.val2;
+    const discussion_id = req.body.discussion_id;
+    const course_id = req.body.course_id;
+    const creted_by = "user" //TODO Once the page has been created change to actually get user data, 
+
+    let date = new Date;
+    const created_on = date.toString();
+
+    const title = req.body.title;
+    const body = req.body.body;
 
     const connection = await mysql.getConnection();
 
     await connection.execute(
         //TODO is the table correct?
-        `INSERT INTO rng_discussion (arg1, arg2) 
-                    VALUES ("${val1}" , "${val2}")`
+        `INSERT INTO rng_discussion (discussion_id, course_id, created_by, created_on, title, body) 
+                    VALUES ("${discussion_id}" , "${course_id}" , "${creted_by}" , "${created_on}" , 
+                    "${title}" , "${body}")`
     );
 
     res.send(`Inserted discussion into DB`);
