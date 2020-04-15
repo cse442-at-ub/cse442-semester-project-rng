a1137d2c06d96b4949317a09426309c226b3fff9
rickyyy72 <ricky.weng89@gmail.com>
2020-04-15 08:29:38 -0400

    Display enrolled courses to students #88
    Students should see the courses they're enrolled in on their courses page

--- backend/src/routes/courses.js
+++ backend/src/routes/courses.js
@@ -10,16 +10,16 @@ router.get('/courses/create', auth.isLoggedIn, (req, res) => {
   const user = req.session.user;
   const isInstructor = user.user_type === 'Instructor';
   if (!isInstructor) {
-    return res.send('You need to be an instructor to access this page')
+    return res.send('You need to be an instructor to access this page');
   }
-  res.render('create_course')
-})
+  res.render('create_course');
+});
 
 router.post('/courses/enroll', auth.isLoggedIn, CoursesController.enrollCourse);
 
 router.get('/courses/enroll', auth.isLoggedIn, (req, res) => {
-  res.render('enroll_course')
-})
+  res.render('enroll_course');
+});
 
 router.get('/courses', auth.isLoggedIn, async (req, res) => {
   const user = req.session.user;
@@ -27,20 +27,58 @@ router.get('/courses', auth.isLoggedIn, async (req, res) => {
 
   const connection = await mysql.getConnection();
   const [
-    coursesQueryResult
+    coursesQueryResult,
   ] = await connection.execute(
     'SELECT * FROM `rng_courses` where `instructor` = ?',
     [user.user_id]
   );
 
-  const instructor = user.first_name + " " + user.last_name;
+  const instructor = user.first_name + ' ' + user.last_name;
+
+  const [
+    enrolledCoursesQueryResult,
+  ] = await connection.execute(
+    'SELECT * FROM `rng_enrollment` where `user_id` = ?',
+    [user.user_id]
+  );
+
+  const enrolledCourses = [];
+  let row;
+  for (row of enrolledCoursesQueryResult) {
+    const [
+      courseQueryResult,
+    ] = await connection.execute(
+      'SELECT * FROM `rng_courses` where `course_id` = ?',
+      [row.course_id]
+    );
+
+    const instructorID = courseQueryResult[0].instructor;
+
+    const [
+      instructorQueryResult,
+    ] = await connection.execute(
+      'SELECT * FROM `rng_users` where `user_id` = ?',
+      [instructorID]
+    );
+
+    const enrolledCourse = {
+      course_name: courseQueryResult[0].course_name,
+      course_term: courseQueryResult[0].course_term,
+      instructor:
+        instructorQueryResult[0].first_name +
+        instructorQueryResult[0].last_name,
+    };
+
+    enrolledCourses.push(enrolledCourse);
+  }
 
   res.render('courses', {
     isInstructor,
     instructor,
     school: user.school,
-    courses: coursesQueryResult
+    instructorCourses: coursesQueryResult,
+    enrolledCourses: enrolledCourses,
   });
-})
+});
 
 module.exports = router;
