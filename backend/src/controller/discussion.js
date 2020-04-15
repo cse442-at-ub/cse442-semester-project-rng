90c32d9fada02d9b29c76e784c6725bdce740121
Daniel Brittain <dmbritta@buffalo.edu>
2020-04-15 18:03:26 -0400

    #79 Finished updating code. Tested and correctly inserts into database.

--- backend/src/controller/discussion.js
+++ backend/src/controller/discussion.js
@@ -3,21 +3,23 @@ const bodyParser = require('body-parser');
 
 module.exports.postDiscussion = async (req, res) => {
 
-  let date = new Date();
+  let date = new Date().toISOString().slice(0, 19).replace('T', ' ');
   const created_on = date.toString();
+
+  const user = req.session.user;
 
   const course_id = req.params.courseID;
   const user_id = user.user_id;
-  const title = req.body.discussion-title;
-  const body = req.body.discussion-content;
+  const title = req.body.title;
+  const body = req.body.body;
 
   const connection = await mysql.getConnection();
 
   await connection.execute(
-    `INSERT INTO rng_discussion (course_id, created_by, created_on, title, body) 
-                    VALUES ("${course_id}" , "${user_id}" , "${created_on}" , 
+    `INSERT INTO rng_discussions (course_id, created_by, created_on, title, body) 
+                    VALUES ("${course_id}" , "${user_id}" , "${date}" , 
                     "${title}" , "${body}")`
   );
 
-  res.send(`Inserted discussion into DB`);
+  res.send(`Inserted discussion into DB ${date}`);
 };
