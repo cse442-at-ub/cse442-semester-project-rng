c4247c09310f15dcf80eb39e9f7786adc458c944
rickyyy72 <ricky.weng89@gmail.com>
2020-04-15 07:48:57 -0400

    Stop users from enrolling in the same course #87
    If a user is already enrolled in a course they should not be able to enroll in the same course again

--- backend/src/controller/courses.js
+++ backend/src/controller/courses.js
@@ -4,10 +4,10 @@ module.exports.createCourse = async (req, res) => {
   const user = req.session.user;
   const isInstructor = user.user_type === 'Instructor';
   if (!isInstructor) {
-    return res.send('You need to be an instructor to perform this action')
+    return res.send('You need to be an instructor to perform this action');
   }
 
-  const instructorID = user.user_id
+  const instructorID = user.user_id;
   const name = req.body.name;
   const term = req.body.term;
 
@@ -17,7 +17,7 @@ module.exports.createCourse = async (req, res) => {
                 VALUES ("${instructorID}" , "${name}" , "${term}")`
   );
 
-  res.redirect('/courses')
+  res.redirect('/courses');
 };
 
 module.exports.enrollCourse = async (req, res) => {
@@ -25,10 +25,11 @@ module.exports.enrollCourse = async (req, res) => {
   const ID = user.user_id;
   const code = req.body.code;
 
+  const connection = await mysql.getConnection();
   const [
-    userQueryResult
+    userQueryResult,
   ] = await connection.execute(
-    'SELECT * FROM `rng_courses` where `course_code` = ?',
+    'SELECT * FROM `rng_courses` where `course_id` = ?',
     [code]
   );
 
@@ -36,13 +37,23 @@ module.exports.enrollCourse = async (req, res) => {
     return res.send('There is no course with that code');
   }
 
-  const course_id = userQueryResult[0];
+  const course_id = userQueryResult[0].course_id;
 
-  const connection = await mysql.getConnection();
+  const [
+    enrollmentQueryResult,
+  ] = await connection.execute(
+    'SELECT * FROM `rng_enrollment` where `user_id` = ? AND`course_id` = ?',
+    [ID, code]
+  );
+
+  if (enrollmentQueryResult.length !== 0) {
+    return res.send('You are already enrolled in the course');
+  }
+
   await connection.execute(
     `INSERT INTO rng_enrollment (user_id, course_id) 
                 VALUES ("${ID}" , "${course_id}")`
   );
 
-  res.redirect('/courses')
-}
+  res.redirect('/courses');
+};
